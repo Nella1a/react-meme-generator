10294a6f257fa6c8fe7cd6178e9c01c4b41d70ca
Nella1a <m.allen@gmx.at>
2022-12-30 13:55:48 +0100

    Refactor Component: Add childcomp and state

--- src/App.js
+++ src/App.js
@@ -1,30 +1,29 @@
-import './App.css';
 /** @jsxImportSource @emotion/react */
 import { css } from '@emotion/react';
 import React, { useEffect, useState } from 'react';
-import DisplayTemplates from './components/display-templates/display_templates.component';
+import DisplayMemeGallery from './components/display-meme_galery/display_meme_gallery.component';
+import GenerateMeme from './components/generate-meme/generate_meme';
 import SearchBox from './components/search-box/search_box.component';
+
+const container = css`
+display: grid;
+grid-template-columns: repeat(2, 1fr) ;
+gap: 1rem;
+
+`;
 
 const sectionStyle = css`
  display: grid;
- grid-template-columns: repeat(5, 1fr);
- gap: .3rem;
-
+ grid-template-columns: repeat(4, 1fr);
+ //gap: .3rem;
+ width: 50%;
 `;
 
 
-const memeSize = css`
-  width: 150px;
-  height: 150px;
-`;
-
-const displayImg = css`
-  width:100%;
-  height: auto;
-`;
 
 
-function App() {
+
+const App = () => {
   // const [topText, setTopText] = useState('');
   // const [botText, setBotText] = useState('');
   // const [specificMeme, setSpecificMeme] = useState('');
@@ -34,14 +33,18 @@ function App() {
 
   const [memeTemplates, setMemeTemplates] = useState([]);
   const [boxSearch, setBoxSearch] = useState("");
+  const [userMemeChoice, setUserMemeChoice] = useState("");
+  const [preview, setPreview] = useState(false);
+
+  console.log("render")
 
 
   useEffect(() => {
+    console.log("uE - initial fetch gallery")
     fetch('https://api.memegen.link/templates')
     .then((response) => response.json())
     .then((data) => setMemeTemplates(data))
-    .catch((error) => alert(error));
-
+    .catch((error) => console.log(error));
   }, []);
 
 
@@ -49,22 +52,28 @@ function App() {
     setBoxSearch(event.target.value.toLocaleLowerCase())
  }
 
-  const filterMemes = memeTemplates.filter((meme) =>
+const onClickMemeChoice = (event) => {
+  console.log("chooseMeme",event)
+  setUserMemeChoice(event)
+  setPreview(false);
+}
+
+const filterMemes = memeTemplates.filter((meme) =>
     meme.name.toLocaleLowerCase().includes(boxSearch)
     )
 
-
-
-
   return (
-    <section >
-      <SearchBox onChangeHandler={onChangeHandler} />
-      <article css={sectionStyle}>
-      {filterMemes.map((template) =>
-      <DisplayTemplates template={template} key={template.id}/>
-      )}
-    </article>
-    </section>
+    <div>
+       <SearchBox onChangeHandler={onChangeHandler} />
+       <section css={container}>
+          <article css={sectionStyle}>
+            {filterMemes.map((template) =>
+            <DisplayMemeGallery template={template} key={template.id} onClickMemeChoice={onClickMemeChoice}/>
+            )}
+          </article>
+        {userMemeChoice && <GenerateMeme userMemeChoice ={userMemeChoice} setUserMemeChoice={setUserMemeChoice} preview={preview} setPreview={setPreview}/> }
+      </section>
+   </div>
 
   );
 }
