78a6fe9f864ec801fc4f5b95a1adf8a7deb2bbe5
Nella1a <m.allen@gmx.at>
2022-12-30 13:57:10 +0100

    Add css

--- src/components/search-box/search_box.component.js
+++ src/components/search-box/search_box.component.js
@@ -1,13 +1,24 @@
-function SearchBox({onChangeHandler}){
+/** @jsxImportSource @emotion/react */
+import { css } from '@emotion/react';
+
+const searchBoxContainer = css`
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  margin-top: 1rem;
+  margin-bottom: 1rem;
+`;
+
+const SearchBox = ({onChangeHandler}) => {
+  console.log("render search-box")
   return(
-  <div>
+  <div css={searchBoxContainer}>
     <input
     type="search"
     placeholder="search meme"
     onChange={onChangeHandler}
     />
-
   </div>)
 }
 
-export default SearchBox
+export default SearchBox;
